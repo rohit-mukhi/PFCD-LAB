09b143a91c79436d7fbb56e66c09342370f04520
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-09-20 14:57:49 +0530

    Create Question_12.py

---
+++ Sept_20_2025/Question_12.py
@@ -0,0 +1,12 @@
+import math as mt
+
+print("Enter radius")
+radius = int(input())
+print("Area of circle is : ")
+area = 0.0
+area = mt.pi * radius * radius
+print(area)
+volume = 0.0
+volume = (4/3) * mt.pi * radius * radius * radius
+print("Volume of sphere: ")
+print(volume)
