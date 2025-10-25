8efd119a0152ab95c7c4c1ef0f5e8288658094c2
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-10-25 14:37:35 +0530

    Create Question_1.py

---
+++ Assignment_4/Question_1.py
@@ -0,0 +1,18 @@
+import statistics as st
+
+arr = []
+
+number_of_elements = int(input("Enter number of elements: "))
+
+for idx in range(0, number_of_elements):
+    val = int(input("Enter an integer: "))
+    arr.append(val)
+
+sum_of_elements = sum(arr)
+average = st.mean(arr)
+
+print(f"The sum of all elements is: {sum_of_elements}")
+print(f"The average is: {average}")
+
+
+
