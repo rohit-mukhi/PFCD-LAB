ace814a34676daf179c4a1eee7368e1a3a87e861
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-10-25 14:48:53 +0530

    Update Question_1.py

--- Assignment_4/Question_1.py
+++ Assignment_4/Question_1.py
@@ -10,6 +10,7 @@ arr = [random.randint(1,100) for _ in range(number_of_elements)]
 sum_of_elements = sum(arr)
 average = st.mean(arr)
 
+print(f"The list is: {arr}")
 print(f"The sum of all elements is: {sum_of_elements}")
 print(f"The average is: {average}")
 
