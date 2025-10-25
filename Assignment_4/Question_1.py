f2010659bced90f6da93e5293c9528bc4a67b48e
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-10-25 14:45:36 +0530

    Update Question_1.py

--- Assignment_4/Question_1.py
+++ Assignment_4/Question_1.py
@@ -1,20 +1,20 @@
 # The sum and average of numbers in a list
 
 import statistics as st
-
-arr = []
+import random
 
 number_of_elements = int(input("Enter number of elements: "))
 
-for idx in range(0, number_of_elements):
-    val = int(input("Enter an integer: "))
-    arr.append(val)
+arr = [random.randint(1,100) for _ in range(number_of_elements)]
 
 sum_of_elements = sum(arr)
 average = st.mean(arr)
 
 print(f"The sum of all elements is: {sum_of_elements}")
 print(f"The average is: {average}")
+
+
+
 
 
 
